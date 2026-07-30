53b10b171b81ae5a9648e479128789b392d2b673
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2026-07-30 16:41:54 +0100

    removed env log

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,7 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-console.log(process.env.CLOUDINARY_CLOUD_NAMEs);
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'd1xj0g7qf',
   api_key: process.env.CLOUDINARY_API_KEY || '<your_api_key>',
